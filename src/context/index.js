8e3328c4a735b54a9c9d5ace6be06c4de0578b55
Josh <zylo.codes@gmail.com>
2020-09-29 19:12:01 -0500

    Create store

---
+++ src/context/index.js
@@ -0,0 +1,119 @@
+import React, { createContext, useContext, useReducer } from 'react'
+
+const lightState = {
+  mode: 'light',
+  notes: [
+    {
+      letter: 'a',
+      note: 'C4',
+      color: '#345995',
+    },
+    {
+      letter: 's',
+      note: 'D4',
+      color: '#e40066',
+    },
+    {
+      letter: 'd',
+      note: 'E4',
+      color: '#03cea4',
+    },
+    {
+      letter: 'f',
+      note: 'F4',
+      color: '#f4b333',
+    },
+    {
+      letter: 'g',
+      note: 'G4',
+      color: '#fb4d3d',
+    },
+    {
+      letter: 'h',
+      note:  'A4',
+      color: '#DABECA',
+    },
+    {
+      letter: 'j',
+      note:  'B4',
+      color: '#ACACDE',
+    }, 
+    {
+      letter: 'k',
+      note:  'C5',
+      color: '#87FF65',
+    }
+  ]
+}
+
+const darkState = {
+  mode: 'dark',
+  notes: [
+    {
+      letter: 'a',
+      note: 'E4',
+      color: '#251e3e',
+    },
+    {
+      letter: 's',
+      note: 'F#4',
+      color: '#451e3e',
+    },
+    {
+      letter: 'd',
+      note: 'G4',
+      color: '#651e3e',
+    },
+    {
+      letter: 'f',
+      note: 'A4',
+      color: '#851e3e',
+    },
+    {
+      letter: 'g',
+      note: 'B4',
+      color: '#007a80',
+    },
+    {
+      letter: 'h',
+      note:  'C4',
+      color: '#fb4d3d',
+    },
+    {
+      letter: 'j',
+      note:  'D4',
+      color: '#3EAB3B',
+    }, 
+    {
+      letter: 'k',
+      note:  'E5',
+      color: '#03cea4',
+    }
+  ]
+}
+
+const AppContext = createContext(lightState)
+const { Provider } = AppContext
+
+// reducer
+const reducer = (state, action) => {
+  switch(action.type) {
+    default:
+      return state
+  }
+}
+
+// provider
+const AppContextProvider = props => {
+  const [state, dispatch] = useReducer(reducer, lightState)
+
+  return <Provider value={{state, dispatch}}>{props.children}</Provider>
+}
+
+// helper
+const useAppContext = () => {
+  return useContext(AppContext)
+}
+
+// export
+export { AppContextProvider, useAppContext }
